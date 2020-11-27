049833c7f2afc70c8ba8d8cebc09440680e749db
Sepehr farrokhnia <sepehrfarrokhnia@gmail.com>
2020-11-27 05:24:23 +0330

    Update asli.js

--- server/Controllers/todo/asli.js
+++ server/Controllers/todo/asli.js
@@ -6,7 +6,7 @@ module.exports = {
             .findAll({
                 attributes: ['title', 'score'],
                 order: [
-                    ['score'],
+                    ['score', 'DESC'],
                 ],
             }).then(rsu => {
                 res.json({
